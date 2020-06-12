b996b1ed86c42a9fa82eceaa65d6087014826021
최우영 <djdj7087@naver.com>
2020-06-12 22:44:34 +0900

    0612 Week12 FileSystem

--- file/dup.c
+++ file/dup.c
@@ -1,5 +1,5 @@
 //
-// Created by djdj7 on 2020-06-03.
+// Created by djdj7 on 2020-06-05.
 //
 
 #include <stdio.h>
@@ -11,24 +11,19 @@ int main() {
 
     char *f_name = "test.txt";
     int fd1, fd2, count;
-    char buf[30];
+    char buf[30] = {'\0'};
 
     fd1 = open(f_name, O_RDONLY);
-    fd2 = open(f_name, O_RDONLY);
+    fd2 = dup(fd1);
 
     printf("fd1: %d\n", fd1);
-    printf("fd2: %d\n", fd2);
+
     count = read(fd1, buf, 12);
     printf("fd1's first printf: %s\n", buf);
     lseek(fd1, 1, SEEK_CUR);
-    count = read(fd1, buf, 12);
-    printf("fd1's second printf: %s\n", buf);
 
     count = read(fd2, buf, 12);
     printf("fd2's first printf: %s\n", buf);
-    lseek(fd2, 1, SEEK_CUR);
-    count = read(fd2, buf, 12);
-    printf("fd2's second printf: %s\n", buf);
 
     close(fd1);
     close(fd2);
