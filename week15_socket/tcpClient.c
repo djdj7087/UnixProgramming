2fe372b1b5cb3f05465b404111bb5774722b3628
wooyoung <djdj7087@naver.com>
2020-06-22 05:34:05 -0700

    running on vmware

---
+++ week15_socket/tcpClient.c
@@ -0,0 +1,16 @@
+//
+// Created by wooyoung on 6/22/20.
+//
+
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#define BUF_SIZE 1024
+
+int main(int argc, char **argv) {
+    return 0;
+}
---
+++ week15_socket/tcpMultiForkServer.c
@@ -0,0 +1,17 @@
+//
+// Created by wooyoung on 6/22/20.
+//
+
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+
+    /* argc means number of arguments. */
+    printf("argc: %d\n", argc);
+    return 0;
+}
