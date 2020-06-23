8674461bf8a92fdbb07e01b60f7ac1caadd3cf3f
최우영 <djdj7087@naver.com>
2020-06-23 15:46:45 +0900

    0623 socket

--- week15_socket/tcpClient.c
+++ week15_socket/tcpClient.c
@@ -12,5 +12,32 @@
 #define BUF_SIZE 1024
 
 int main(int argc, char **argv) {
+    int client_socket;                  // Define Client Socket.
+    struct sockaddr_in client_address;  // Define socket address internet structure
+
+    if (argc != 3) {
+        printf("Usage: %s (IP_Address) (Port)\n", argv[0]);
+        exit(0);
+    }
+
+    if ((client_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+        perror("socket error: ");
+        exit(0);
+    }
+
+    printf("[Debug] client socket: %d\n", client_socket);
+
+    client_address.sin_family = AF_INET;
+    client_address.sin_addr.s_addr = inet_addr(argv[1]);
+    client_address.sin_port = htons(argv[2]);
+
+    if (connect(client_socket, (struct sockaddr *)&client_address,
+            sizeof(client_address)) < 0) {
+        perror("connect error: ");
+        exit(0);
+    }
+
+    close(client_socket);
+
     return 0;
 }
--- week15_socket/tcpMultiForkServer.c
+++ week15_socket/tcpMultiForkServer.c
@@ -11,7 +11,51 @@
 
 int main(int argc, char **argv) {
 
+    // Define socket address internet structure.
+    struct sockaddr_in server_address;
+    struct sockaddr_in client_address;
+
+    int server_socket;
+
+    int client_socket;
+    int client_address_size;
+
     /* argc means number of arguments. */
-    printf("argc: %d\n", argc);
+
+    if (argc != 2) {
+        printf("usage: %s (Port)\n", argv[0]);
+        exit(0);
+    }
+
+    if ((server_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+        perror("socket error: ");
+        exit(0);
+    }
+
+    printf("[Debug] server_socket: %d\n", server_socket);
+
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_address.sin_port = htons(atoi(argv[1]));
+
+    if (bind(server_socket, (struct sockaddr *)&server_address,
+             sizeof(server_address)) < 0) {
+        perror("bind error: ");
+        exit(0);
+    }
+
+    if (listen(server_socket, 5) < 0) {
+        perror("bind error: ");
+        exit(0);
+    }
+
+    client_address_size = sizeof(client_address);
+    client_socket = accept(server_socket, (struct sockaddr *)&client_address,
+            &client_address_size);
+
+    printf("[Debug] There is a connection request from a client\n");
+
+    close(server_socket);
+
     return 0;
 }
